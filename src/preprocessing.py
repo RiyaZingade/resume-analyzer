9a53e163bd2d57ab2cb910a314b4e2d6a44144f5
Riya Zingade <riya.zingade@gmail>
2026-02-13 15:29:14 -0800

    started preprocessing

---
+++ src/preprocessing.py
@@ -0,0 +1,10 @@
+#acceptable things
+"""
+allowed --> numbers, letters, +, #, :, -, \, . --> thats it, everything else removed
+"""
+
+from typing import List
+
+
+# def normalize(resume: str) -> List[str]:
+    
